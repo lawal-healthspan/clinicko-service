146e9be58d70279dc1d774948b7337ca2462e2eb
Lawal Olaotan <lawal@healthspaninnovations.co.uk>
2024-12-30 11:09:39 +0000

    Commit continues update on serverless

--- src/functions/check.ts
+++ src/functions/check.ts
@@ -1,39 +1,39 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { z } from 'zod';
-import clinics from '../helpers/business';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { z } from "zod";
+import clinics from "../helpers/business";
 
 const eventBodySchema = z.object({
-  email: z.string().email({message:"userId must be at least 26 characters"}),
-  apiKey: z.string().min(16,{message:"API key must be at least 76 characters"}),
+  email: z.string().email({ message: "userId must be at least 26 characters" }),
+  apiKey: z
+    .string()
+    .min(16, { message: "API key must be at least 76 characters" }),
 });
 
-
-export const hello:APIGatewayProxyHandler = async (event, _context) => {
-  try{
-    const clinic = clinics()
-    const requestBody = JSON.parse(event.body || "{}")
+export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  try {
+    const clinic = clinics();
+    const requestBody = JSON.parse(event.body || "{}");
     const validatedData = eventBodySchema.parse(requestBody);
-    const { apiKey } = validatedData
-    
-    const response = await clinic.businesses(apiKey)
+    const { apiKey } = validatedData;
+
+    const response = await clinic.businesses(apiKey);
 
     return {
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
       },
       statusCode: 200,
-      
+
       body: JSON.stringify(response),
     };
-    
-  }catch(error){
+  } catch (error) {
     if (error instanceof z.ZodError) {
       return {
         statusCode: 400,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify({
           error: error.errors, // Validation errors
@@ -44,8 +44,8 @@ export const hello:APIGatewayProxyHandler = async (event, _context) => {
     return {
       statusCode: 500,
       headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
       },
       body: JSON.stringify({
         error: "Internal server error",
@@ -54,6 +54,6 @@ export const hello:APIGatewayProxyHandler = async (event, _context) => {
   }
 };
 
-
 export default hello;
 
+// cloned the serverless
